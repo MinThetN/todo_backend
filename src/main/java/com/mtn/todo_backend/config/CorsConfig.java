8a43c581c691028fdb1330ba6f3917970f58ddf8
MinThetN <minthetn15@gmail.com>
2025-02-11 18:20:57 +0700

    fixed

--- src/main/java/com/mtn/todo_backend/config/CorsConfig.java
+++ src/main/java/com/mtn/todo_backend/config/CorsConfig.java
@@ -3,15 +3,14 @@ package com.mtn.todo_backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Allow all endpoints to accept requests from frontend running on localhost:3000
         registry.addMapping("/**")
-                .allowedOrigins("https://todo-task-ecru-pi.vercel.app/")
+                .allowedOrigins(System.getenv("FRONTEND_URL")) // Read from environment variable
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);  // If necessary, adjust this according to your requirements
+                .allowCredentials(true);
     }
-
 }
